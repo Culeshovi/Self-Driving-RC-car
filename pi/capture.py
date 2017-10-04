088a803f151891f4ed58f64099df949f7ef16a9b
culeshovi <culeshovi@hotmail.com>
2017-10-04 21:36:31 +0530

    cap

---
+++ pi/capture.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import os
+cap = cv2.VideoCapture(1) #If webcam does'nt work set it to -1 or 1
+count=0
+path_output_dir='\data'
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    cv2.imwrite(os.path.join(path_output_dir,"frame%d.jpg")%count,frame)
+    count+=1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.releas
+cv2.destroyAllWindows()
