80230f3d142b9726419876ffd6564c182f9698f2
culeshovi <culeshovi@hotmail.com>
2017-10-07 08:40:16 +0530

    RohanKaJalwa

--- pi/capturethread.py
+++ pi/capturethread.py
@@ -1,12 +1,15 @@
 import cv2
 import os
-cap = cv2.VideoCapture(0) #If webcam does'nt work set it to -1 or 1
+from imutils.video import WebcamVideoStream
+import imutils
+
+cap = WebcamVideoStream(src=0).start()#If webcam does'nt work set it to -1 or 1
 count=0
 path_output_dir='\data'
 e1 = cv2.getTickCount()
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    frame = cap.read()
     
     cv2.imwrite(os.path.join(path_output_dir,"frame%d.jpg")%count,frame)
     count+=1
@@ -16,5 +19,5 @@ while(True):
 	t = (e2 - e1)/cv2.getTickFrequency()
 	print (1.0/t)
 # When everything done, release the capture
-cap.release()
+cap.stop()
 cv2.destroyAllWindows()
