543095306066607017a278101a3d105e864c1679
culeshovi <culeshovi@hotmail.com>
2017-10-27 19:13:24 +0530

    NewCollector

---
+++ pi/client 2.py
@@ -0,0 +1,22 @@
+import socket               # Import socket module
+import cv2
+import numpy as np
+import time
+import pickle
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         # Create a socket object
+host = socket.gethostname() # ipManSaysHi...Awwwwww
+port = 5555               # Reserve a port for your service.
+s.connect((host, port))
+cap=cv2.VideoCapture(0)
+while True:
+	ret,frame=cap.read()
+	rval,imgencode=cv2.imencode(".jpg",frame,[1,90])
+	data1=pickle.dumps(imgencode) #Pickkkkkyyyyyyyyyyyyyyy..........Yahoooooooooooooooooo
+	s.sendall(data1)
+	
+
+cap.release()
+s.close()
+
+
+
---
+++ pi/server2.py
@@ -0,0 +1,31 @@
+import socket
+import cv2
+import numpy as np  
+import pickle   
+import os     # Import socket module
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         # Create a socket object
+host = socket.gethostname() #ipManSaysHi...Awwwwww
+port = 5555
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)               # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+s.listen(5)
+count=0  
+c, addr = s.accept()
+path_output_dir='/home/culeshovi/Desktop/'     # Establish connection with client.
+print ('Got connection from', addr)
+ # Now wait for client connection.
+while True:
+	e1 = cv2.getTickCount()
+	data=c.recv(1024*1024)
+	data=pickle.loads(data)
+	dec=cv2.imdecode(data,1)
+	cv2.imshow('frame2',dec)
+	cv2.imwrite(os.path.join(path_output_dir+k,"frame%d.jpg")%count,dec)
+	count+=1
+	if count==100 or cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
+
+
+c.close()
+cv2.destroyAllWindows()      
