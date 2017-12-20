6de0c53b566a20c79b05eb3f8b827047cf5be85b
culeshovi <culeshovi@hotmail.com>
2017-12-20 11:02:39 +0530

    New Change

---
+++ pi/getkeys.py
@@ -0,0 +1,22 @@
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys, termios # import termios now or else you'll get the Unix version on the Mac
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+def getKey():
+    inkey = _GetchUnix()
+    import sys
+    for i in range(sys.maxsize):
+        k=inkey()
+        if k >'':
+            break
+    return k
